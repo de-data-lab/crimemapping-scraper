813ff1a0c62683e5adf4cea53691be4b0c1fe606
Nami Sunami <naoyuki.sunami@gmail.com>
2022-05-20 14:54:04 -0400

    Implement modules

--- scrape.py
+++ scrape.py
@@ -1,12 +1,13 @@
-import re
 import pandas as pd
-from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from dateutil.relativedelta import relativedelta
+# Local modules
+from utils.PageMetadata import PageMetadata
+from utils.change_daterange import change_daterange
 
 
 # Selenium options 
@@ -19,37 +20,15 @@ driver = webdriver.Chrome(options=options)
 driver.get("https://www.crimemapping.com/map/agency/522")
 
 # Change the date range ----------------------------------------------------------------------------------------
-## Click the "When" button
-when_button = driver.find_element(By.XPATH, '//*[@id="filtersWhen"]')
-when_button.click()
+initial_dates = change_daterange(driver)
 
-## Click the "Custom Time Range"
-custom_time_link = driver.find_element(By.XPATH, '//*[@id="whenPanel"]/ul/li[9]/div/a')
-custom_time_link.click()
-
-## Get the current date
-to_date_input = driver.find_element(By.XPATH, '//*[@id="dateTo"]')
-to_date = datetime.strptime(to_date_input.get_attribute('value'), '%m/%d/%Y')
-### NOTE: We can only query up to 1000 records. Probably scraping one month at a time would be reasonable
-
-## Get the max date available on the website (6 months)
-oldest_date = to_date - relativedelta(months=6)
-
-## Set the target date for the current query
-from_date_target = to_date - relativedelta(months=1)
-from_date_input = driver.find_element(By.XPATH, '//*[@id="dateFrom"]')
-from_date_input.clear()
-from_date_input.send_keys(from_date_target.strftime('%m/%d/%Y'))
-# driver.save_screenshot('test.png')
-
-## Click "Apply"
-apply_button = driver.find_element(By.XPATH, '//*[@id="customDate"]/a')
-apply_button.click()
+## Get the oldest date available on the website (6 months)
+oldest_date = initial_dates.get('to_date') - relativedelta(months=6)
+latest_date = initial_dates.get('to_date')
 
 # Open the Report view ----------------------------------------------------------------------------------------
-# Find the display report button: #displayReports
+# Find and click the display report button
 display_report = driver.find_element(By.ID, 'displayReports')
-# Click the display report button
 display_report.click()
 
 # Wait for the report window to load
@@ -60,41 +39,21 @@ try:
 finally:
     pass
 
-# Save screenshot for testing
-# driver.save_screenshot('selenium.png')
-
-def get_items_text():
-    # Get the number of items
-    items_str = driver.find_element(By.XPATH, '//*[@id="CrimeIncidents"]/div[3]/span')
-    items_text = items_str.text
-    return items_text
-
-def get_total_items(items_text = get_items_text()):
-    # Get the total number of items in the table 
-    match_total = re.compile("[0-9]+(?=( items))")
-    total_items = match_total.search(items_text).group(0)
-    return int(total_items)
-
-def get_max_rendered(items_text = get_items_text()):
-    # Get the max number of items rendered in the table
-    match_max_rendered = re.compile('[0-9]+(?=( of))')
-    max_rendered = match_max_rendered.search(items_text).group(0)
-    return int(max_rendered)
-
-max_rendered = get_max_rendered(get_items_text())
-total_items =  get_total_items(get_items_text())
-print("max rendered: " + str(max_rendered) )
-print("total items: " + str(total_items) )
-
-# Paginated scraping begins 
-current_page = 0
+# Get current page's metadata
+cur_page_metadata = PageMetadata(driver)
+cur_page_metadata.update_metadata()
+print("max rendered: " + str(cur_page_metadata.max_rendered) )
+print("total items: " + str(cur_page_metadata.total_items) )
 
 # Container pd data frame to collect results
 output_df = pd.DataFrame()
 
 # TODO: Repeat the set-date routine here
 
-while(total_items > max_rendered):
+# Paginated scraping begins 
+current_page = 0
+
+while(cur_page_metadata.total_items > cur_page_metadata.max_rendered):
     current_page = current_page + 1
     print("Processing page: " + str(current_page))
     # Get the rendered table
@@ -118,19 +77,23 @@ while(total_items > max_rendered):
         )
     except: 
         print("Error on page: " + current_page)
-    finally:
-        # Update the numbers
-        items_text = get_items_text()
-        max_rendered = get_max_rendered(items_text)
-        total_items = get_total_items(items_text)
-        # driver.save_screenshot('page-' + str(current_page) + '.png')
-        print("max rendered: " + str(max_rendered) )
-        print("total items: " + str(total_items) )
+
+    # Update the metadata
+    cur_page_metadata.update_metadata()
+
+    # driver.save_screenshot('page-' + str(current_page) + '.png')
+    print("max rendered: " + str(cur_page_metadata.max_rendered) )
+    print("total items: " + str(cur_page_metadata.total_items) )
+
+# Close the report view (Back to Map)
+back_to_map_element = driver.find_element(By.XPATH, '//*[@id="reportContainer"]/a[3]')
+back_to_map_element.click()
+
+# TODO: Change the date and loop through
+
 driver.quit()
 
 # Save the output
 output_df.to_csv("output.csv")
 
-# TODO:
-# - Add a header to the pd data frame  
-# - Select the range of dates to max (6 months)
+# TODO: Add a header to the pd data frame  
